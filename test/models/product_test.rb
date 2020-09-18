f6af92e0216b140cd2819ad81f44ceb89f171059
Sam Partington <sampart@github.com>
2020-09-18 13:18:46 +0100

    Friday afternoon

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -49,4 +49,13 @@ class ProductTest < ActiveSupport::TestCase
              "#{image_url} shouldn't be valid"
     end
   end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(title: products(:ruby).title,
+                          description: "yyy",
+                          price: 1,
+                          image_url: "fred.gif")
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
+  end
 end
