11c20db0c863d94f9c3079beb201e6ac9362b8ae
Sam Partington <sampart@github.com>
2020-10-01 09:51:55 +0100

    Thursday morning

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @cart = Cart.new
+  end
+
+  test "add unique products to cart" do
+    @cart.add_product(products(:ruby)).save!
+    @cart.add_product(products(:one)).save!
+
+    assert_equal 2, @cart.line_items.size
+  end
+
+  test "add duplicate products to cart" do
+    @cart.add_product(products(:ruby)).save!
+    @cart.add_product(products(:ruby)).save!
+    @cart.add_product(products(:one)).save!
+
+    assert_equal 2, @cart.line_items.size
+    assert_equal 2, @cart.line_items.first.quantity
+    assert_equal 1, @cart.line_items.second.quantity
+  end
 end
