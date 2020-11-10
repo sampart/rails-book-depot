d3051dd9436aeb98a5afabe4b6fe7eb49cad8793
Sam Partington <sampart@github.com>
2020-11-10 12:48:43 +0000

    Add basic (unstyled) HTML for who_bought

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  include ActiveModel::Serializers::Xml
+
   has_many :line_items
   has_many :orders, through: :line_items
   before_destroy :ensure_not_referenced_by_any_line_item
