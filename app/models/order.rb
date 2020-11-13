cdd287edb046cf30bf74119ad20a5970b67b28d9
Sam Partington <sampart@github.com>
2020-11-13 10:49:09 +0000

    Make checkout form work with pay type relationship

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,14 +1,8 @@
 class Order < ApplicationRecord
   has_many :line_items, dependent: :destroy
-
-  enum pay_type: {
-    "Check"          => 0,
-    "Credit card"    => 1,
-    "Purchase order" => 2
-  }
+  has_one :pay_type
 
   validates :name, :address, :email, presence: true
-  validates :pay_type, inclusion: pay_types.keys
 
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
