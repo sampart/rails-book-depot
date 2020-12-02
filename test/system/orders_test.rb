d97e2e18616e4484e0938e41241d3f230b59d392
Sam Partington <sampart@github.com>
2020-12-02 16:05:52 +0000

    Add additional payment type system tests

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -67,4 +67,36 @@ class OrdersTest < ApplicationSystemTestCase
     
     assert_selector "#order_routing_number"
   end
+
+  test "credit card number appears" do
+    visit store_index_url
+    click_on 'Add to cart', match: :first
+    click_on 'Checkout'
+    
+    fill_in 'order_name', with: 'Dave Thomas'
+    fill_in 'order_address', with: '123 Main Street'
+    fill_in 'order_email', with: 'dave@example.com'
+    
+    assert_no_selector "#order_credit_card_number"
+    
+    select 'Credit card', from: 'Pay type'
+    
+    assert_selector "#order_credit_card_number"
+  end
+
+  test "purchase order number appears" do
+    visit store_index_url
+    click_on 'Add to cart', match: :first
+    click_on 'Checkout'
+    
+    fill_in 'order_name', with: 'Dave Thomas'
+    fill_in 'order_address', with: '123 Main Street'
+    fill_in 'order_email', with: 'dave@example.com'
+    
+    assert_no_selector "#order_po_number"
+    
+    select 'Purchase order', from: 'Pay type'
+    
+    assert_selector "#order_po_number"
+  end
 end
