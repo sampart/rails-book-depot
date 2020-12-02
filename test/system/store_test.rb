2f93737bc27328143f863f3f977b12e4806583d0
Sam Partington <sampart@github.com>
2020-12-02 16:23:53 +0000

    Add a system test for the highlight of most recent cart item

--- test/system/store_test.rb
+++ test/system/store_test.rb
@@ -23,4 +23,22 @@ class OrdersTest < ApplicationSystemTestCase
     
     assert_no_selector "h2", text: "Your Cart"
   end
+
+  test "Most recently added item in cart is highlighted" do
+    visit store_index_url
+    assert_no_selector ".line-item-highlight"
+
+    click_on "Add to cart", match: :first
+    assert_selector ".line-item-highlight", count: 1
+
+    # the second "Add to cart" button
+    page.all('.price input[type=submit]')[2].click
+    assert_selector ".line-item-highlight", count: 1
+
+    # remove an item from the cart, so it'll reload
+    # with no most recent item
+
+    click_on "-", match: :first
+    assert_no_selector ".line-item-highlight"
+  end
 end
