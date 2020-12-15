28fd6bea130618858a315337fba3f26c45ce1c9d
Sam Partington <sampart@github.com>
2020-12-15 14:39:38 +0000

    Add tests for background jobs

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -1,6 +1,8 @@
 require "application_system_test_case"
 
 class OrdersTest < ApplicationSystemTestCase
+  include ActiveJob::TestHelper
+
   setup do
     @order = orders(:one)
   end
@@ -52,7 +54,10 @@ class OrdersTest < ApplicationSystemTestCase
     assert_text "Order was successfully destroyed"
   end
 
-  test "check routing number appears" do
+  test "check routing number appears and can then submit" do
+    LineItem.delete_all
+    Order.delete_all
+    
     visit store_index_url
     click_on 'Add to cart', match: :first
     click_on 'Checkout'
@@ -66,6 +71,27 @@ class OrdersTest < ApplicationSystemTestCase
     select 'Check', from: 'Pay type'
     
     assert_selector "#order_routing_number"
+
+    fill_in "Routing #", with: "123456"
+    fill_in "Account #", with: "987654"
+
+    perform_enqueued_jobs do
+      click_button "Place Order"
+    end
+
+    orders = Order.all
+    assert_equal 1, orders.size
+    order = orders.first
+    assert_equal "Dave Thomas", order.name
+    assert_equal "123 Main Street", order.address
+    assert_equal "dave@example.com", order.email
+    assert_equal "Check", order.pay_type.description
+    assert_equal 1, order.line_items.size
+
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal ["dave@example.com"], mail.to
+    assert_equal 'Sam Ruby <depot@example.com>', mail[:from].value
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
   end
 
   test "credit card number appears" do
