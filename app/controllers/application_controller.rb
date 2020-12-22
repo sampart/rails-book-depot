d0b5b4ad80122a40b6ed712e3aea285136785cf3
Sam Partington <sampart@github.com>
2020-12-22 09:57:47 +0000

    Enforce login restrictions on admin pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authorize
   rescue_from StandardError, with: :report_error
 
   def report_error(exception)
@@ -10,5 +11,13 @@ class ApplicationController < ActionController::Base
   # But, to be extra sure, check the env again
   def raise_error
     raise(StandardError, "test") if Rails.env.test?
+  end
+
+  protected
+
+  def authorize
+    unless User.find_by(id: session[:user_id])
+      redirect_to login_url, notice: "Please log in"
+    end
   end
 end
