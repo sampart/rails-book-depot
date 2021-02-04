4e87aec65f426077615d8657774f4e27605819a8
Sam Partington <sampart@github.com>
2021-02-04 12:29:27 +0000

    More translation stuff

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -28,7 +28,7 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
       post orders_url, params: { order: { address: @order.address, email: @order.email, name: @order.name, pay_type: pay_types(:credit_card).description } }
     end
 
-    assert_redirected_to store_index_url
+    assert_redirected_to store_index_url(locale: 'en')
   end
 
   test "should show order" do
