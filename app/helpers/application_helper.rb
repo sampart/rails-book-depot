e8368158456343334ccf2ad92a4d87d2092d23cc
Sam Partington <sampart@github.com>
2021-03-03 16:49:41 +0000

    Show prices in Euros in es locale

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,22 @@
 module ApplicationHelper
+
+  DOLLAR_TO_EUR = 1.21
+
   def render_if(condition, record)
     if condition
       render record
     end
+  end
+
+  def localise_usd_number_and_format_as_currency(usd)
+    case I18n.locale
+    when :es
+      unit = "€"
+      price = usd * DOLLAR_TO_EUR
+    else
+      unit = "$"
+      price = usd
+    end
+    number_to_currency(price, unit: unit)
   end
 end
