bfb49cbb866efd97295ff9ff1f2dd89a3552b6fd
Sam Partington <sampart@github.com>
2021-03-03 16:31:13 +0000

    Make use of locale column on store page

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -8,7 +8,7 @@ class StoreController < ApplicationController
     if params[:set_locale]
       redirect_to store_index_url(locale: params[:set_locale])
     else
-      @products = Product.order(:title)
+      @products = Product.where(locale: I18n.locale).or(Product.where(locale: nil)).order(:title)
     end
   end
 end
