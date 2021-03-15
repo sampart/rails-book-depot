5f496dcbc7bc9d4559c988a396c641b1a2d7ef2f
Sam Partington <sampart@github.com>
2021-03-15 13:56:42 +0000

    Send responses to support requests

--- app/controllers/support_requests_controller.rb
+++ app/controllers/support_requests_controller.rb
@@ -6,6 +6,7 @@ class SupportRequestsController < ApplicationController
   def update
     support_request = SupportRequest.find(params[:id])
     support_request.update(response: params.require(:support_request)[:response])
+    SupportRequestMailer.respond(support_request).deliver_now
     redirect_to support_requests_path
   end
 end
