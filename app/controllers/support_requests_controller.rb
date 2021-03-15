dbe649a3527094a9b7fc914e333d359099965135
Sam Partington <sampart@github.com>
2021-03-15 13:42:37 +0000

    Add management of rich text responses; not actually sent yet

--- app/controllers/support_requests_controller.rb
+++ app/controllers/support_requests_controller.rb
@@ -2,4 +2,10 @@ class SupportRequestsController < ApplicationController
   def index
     @support_requests = SupportRequest.all
   end
+
+  def update
+    support_request = SupportRequest.find(params[:id])
+    support_request.update(response: params.require(:support_request)[:response])
+    redirect_to support_requests_path
+  end
 end
